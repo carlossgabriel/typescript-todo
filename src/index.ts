6f4dd453d02f3c563604d3d04a503f0d3e012ce4
carlossgabriel <cgsalbuquerque@gmail.com>
2021-09-17 10:08:46 -0300

    create database with lowdb json

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { TodoItem } from './todoItem'
 import { TodoCollection } from './todoCollection'
 import * as inquirer from 'inquirer'
+import { JsonTodoCollection } from './jsonTodoCollection'
 
 const todos: TodoItem[] = [
   new TodoItem(1, 'Buy Flowers'), new TodoItem(2, 'Get Shoes'),
   new TodoItem(3, 'Collect Tickets'), new TodoItem(4, 'Call Joe', true)]
 
-const collection: TodoCollection = new TodoCollection('Adam', todos)
+// const collection: TodoCollection = new TodoCollection('Adam', todos)
+const collection: TodoCollection = new JsonTodoCollection('Gabriel', todos)
 let showCompleted = true
 
 function displayTodoList (): void {
